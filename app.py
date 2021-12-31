0cd3140e8b642be7260ce52e7213f6006d7eee95
matiaserodriguez <maturod96@gmail.com>
2021-12-31 18:32:39 -0300

    Creates user table.

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from src import app, api
 from src.models.login_model import login_table
 from src.models.repo_model import repo_table
+from src.models.user_model import user_table
 from src.resources.user_resource import UsersResource
 
 
 api.add_resource(UsersResource, '/users', endpoint='users_ep')
 
-
 login_table.create_table()
 repo_table.create_table()
+user_table.create_table()
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,6 +1,7 @@
-from . import Base
-
 from sqlalchemy import Column, Integer, String, Enum
+
+from . import Base
+from tables import CreatesTable
 
 
 class UserModel(Base):
@@ -9,5 +10,7 @@ class UserModel(Base):
     name = Column(String(100), nullable = False, unique=True)
     password = Column(String(255), nullable=False)
     birth = Column(String(100), nullable=False)
-    programming_languajes = Column(Enum(255), nullable=True)
+    programming_languajes = Column(Enum(name="programming_languajes"), nullable=True)
     
+
+user_table = CreatesTable(UserModel)
