4b172c655fb31646a572cb2033f2901455e38895
matiaserodriguez <maturod96@gmail.com>
2021-12-31 18:24:10 -0300

    Creates repo table.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src import app, api
 from src.models.login_model import login_table
+from src.models.repo_model import repo_table
 from src.resources.user_resource import UsersResource
 
 
@@ -7,6 +8,7 @@ api.add_resource(UsersResource, '/users', endpoint='users_ep')
 
 
 login_table.create_table()
+repo_table.create_table()
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
--- src/models/repo_model.py
+++ src/models/repo_model.py
@@ -1,14 +1,19 @@
 from datetime import date
 
+from sqlalchemy.sql.sqltypes import DateTime
+
 from . import Base
 
 from sqlalchemy import Column, Integer, String, Enum, Date
+from tables import CreatesTable
 
 
 class RepoModel(Base):
     __tablename__ = 'repositories'
     id = Column(Integer, primary_key=True)
     proyect_name = Column(String(255), nullable=False)
-    languaje = Column(Enum, nullable=False)
-    creation_date = Column(Date(), default=date.now(), nullable=False)
+    languaje = Column(Enum(name="kinds_of_languajes"), nullable=False)
+    creation_date = Column(DateTime(), default=date, nullable=False)
     description = Column(String)
+
+repo_table = CreatesTable(RepoModel)
