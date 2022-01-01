8faed209e96aa28be8e5795c1bffde5cbafca3b7
matiaserodriguez <maturod96@gmail.com>
2022-01-01 19:15:17 -0300

    Creates obtain_all method that queries all users. Adds corresponding tests

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -5,6 +5,9 @@ from .commit_after_service import commit_after
 
 class UserService(ConnectionService):
 
+    def obtain_all(self):
+        return self._session.query(UserModel).all()
+
     @commit_after
     def insert(self, name, password, birth, languaje):
         new_user = UserModel(name=name, password=password, birth=birth, programming_languaje=languaje)
--- test/test_services/test_user_service.py
+++ test/test_services/test_user_service.py
@@ -7,15 +7,32 @@ from src.services.user_service import user
 
 class TestUserService(unittest.TestCase):
 
-   def tearDown(self):
-      connection.session.query(UserModel).delete() 
-      connection.session.commit()
+    def setUp(self):
+        connection.session.add(UserModel(name='John', password='pass', birth='09-08-2000', programming_languaje='javascript'))
+        connection.session.add(UserModel(name='Juan', password='pass', birth='01-03-1987', programming_languaje='python'))
 
-   def test_insert_user_returns_user_created(self):
-      answer = user.insert("Pasta", "password", "07/09/2008", 'python')
+    def tearDown(self):
+        connection.session.query(UserModel).delete() 
+        connection.session.commit()
 
-      self.assertEqual("Pasta", answer.name)
-      self.assertEqual("password", answer.password)
-      self.assertEqual("07/09/2008", answer.birth)
-      self.assertEqual("python", answer.programming_languaje.name)
-      self.assertIsInstance(answer.id, int)
+    def test_insert_user_returns_user_created(self):
+        answer = user.insert("Pasta", "password", "07/09/2008", 'python')
+
+        self.assertEqual("Pasta", answer.name)
+        self.assertEqual("password", answer.password)
+        self.assertEqual("07/09/2008", answer.birth)
+        self.assertEqual("python", answer.programming_languaje.name)
+        self.assertIsInstance(answer.id, int)
+
+    def test_query_all_users_and_get_values(self):
+        answer = user.obtain_all()
+
+        self.assertEqual('John', answer[0].name)
+        self.assertEqual('pass', answer[0].password)
+        self.assertEqual('09-08-2000', answer[0].birth)
+        self.assertEqual('javascript', answer[0].programming_languaje.name)
+
+        self.assertEqual('Juan', answer[1].name)
+        self.assertEqual('pass', answer[1].password)
+        self.assertEqual('01-03-1987', answer[1].birth)
+        self.assertEqual('python', answer[1].programming_languaje.name)
